f76c2d4c9cd12f45fed6ad936633b2729c41899b
Денис <garaevdenis2019@gmail.com>
2023-07-14 23:35:12 +0500

    in progress

--- src/test/java/tests/IntegerListTest.java
+++ src/test/java/tests/IntegerListTest.java
@@ -2,97 +2,138 @@ package tests;
 
 import org.example.IntegerList;
 import org.example.IntegerListImpl;
+import org.example.exceptions.InvalidIndexException;
 import org.example.exceptions.NullItemException;
+import org.example.exceptions.StorageIsFullException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class IntegerListTest {
-    IntegerList out = new IntegerListImpl();
 
-    @BeforeEach
-    public void setUp() {
-        IntegerList out = new IntegerListImpl();
-    }
+    private IntegerList out = new IntegerListImpl();
+
+
     @Test
     public void ShouldThrowExceptionWhenVaItemIsNull() {
-            assertThrows(NullItemException.class,
-                    ()-> out.add(null));
-        }
-
-//@Test
-//    public void validateSizeTest() {
-//        if (size == storage.length) {
-//            throw new StorageIsFullException();
-//        }
-//    }
-////@Test
-//    public void ShouldThrowExceptionWhenVaIndexIsNull(int index) {
-//        if (index < 0 || size > index) {
-//            throw new InvalidIndexException();
-//        }
-//    }
-
-
-        @Test
-        public void shouldBeEqualWhenAddItem() {
-
-            out.add(1);
-            assertEquals("Hello", out.get(0));
-        }
-
-
-//        @Test
-//        public void shouldBeEqualWhenAddItemAndIndex() {
-//            Integer[] arrays = new Integer[]{"1"};
-//            out.add(0,"1");
-//            assertArrayEquals(arrays, out.get(1));
-
-//        }
-        @Test
-        public void shouldBeEqualWhenSetItemAndIndex() {
-        }
-
-        @Test
-        public void  shouldBeEqualWhenRemoveItem() {
-        }
-
-        @Test
-        public void  shouldBeEqualWhenRemoveIndex() {
-        }
-
-        @Test
-        public void  shouldBeEqualWhenContainsItem() {
-        }
-
-        @Test
-        public void shouldBeEqualWhenIndexOFItem() {
-        }
-
-        @Test
-        public void shouldBeEqualWhenLastIndexOFItem () {
-        }
-
-        @Test
-        public void shouldBeEqualWhenGetIndex () {
-        }
-
-        @Test
-        public void shouldBeEqualsWhenIntegerListOtherList() {
-        }
-
-
-        @Test
-        public void shouldBeEqualWhenIsEmpty() {
-            }
-        @Test
-        public void shouldBeNullWhenClear() {
-                }
-        @Test
-        public void shouldBeEqualWhenToArray() {
-        }
+        assertThrows(NullItemException.class,
+                ()-> out.add(null));
     }
+
+    @Test
+    public void validateSizeTest() {
+
+        assertThrows(StorageIsFullException.class,
+                ()-> out.add(1,10));
+    }
+    @Test
+    public void ShouldThrowExceptionWhenVaIndexIsNull(int index) {
+
+        out.add(0, 10);
+        assertThrows(InvalidIndexException.class,
+                () -> out.get(1));
+
+    }
+
+
+    @Test
+    public void shouldBeEqualWhenAddItem() {
+        out.add(10);
+        assertEquals(10, out.get(0));
+    }
+
+
+    @Test
+    public void shouldBeEqualWhenAddItemAndIndex() {
+        out.add(0,10);
+        assertEquals(10, out.get(0));
+
+    }
+    @Test
+    public void shouldBeEqualWhenSetItemAndIndex() {
+        out.set(0, 10);
+        assertEquals(10, out.get(0));
+    }
+
+    @Test
+    public void  shouldBeEqualWhenRemoveItem() {
+        out.add(0, 10);
+        out.add(1, 20);
+        out.remove(20);
+        assertEquals(10, out.get(0));
+    }
+
+    @Test
+    public void  shouldBeEqualWhenRemoveIndex() {
+        out.add(10);
+        out.add(20);
+        out.remove(1);
+        assertEquals(10, out.get(0));
+    }
+
+    @Test
+    public void  shouldBeEqualWhenContainsItem() {
+        out.add(10);
+        out.add(20);
+        assertTrue( out.contains(10));
+    }
+
+    @Test
+    public void shouldBeEqualWhenIndexOFItem() {
+        assertEquals(-1,out.indexOF(10));
+    }
+
+    @Test
+    public void shouldBeEqualWhenLastIndexOFItem () {
+        out.add( 10);
+        out.add(20);
+        assertEquals(0, out.lastIndexOF(10));
+    }
+
+    @Test
+    public void shouldBeEqualWhenGetIndex () {
+        out.add(10);
+        out.add(20);
+        assertEquals(20, out.get(1));
+    }
+
+    @Test
+    public void shouldBeEqualsWhenIntegerListOtherList() {
+        IntegerList another = new IntegerListImpl();
+        another.add( 10);
+        out.add( 10);
+        assertTrue(another.equals(out));
+    }
+    @Test
+    public void sizeTest() {
+        out.add(10);
+        assertEquals(1, out.size());
+    }
+
+
+    @Test
+    public void shouldBeEqualWhenIsEmpty() {
+        out.add(0, 10);
+        assertFalse( out.isEmpty());
+    }
+    @Test
+    public void shouldBeNullWhenClear() {
+        out.add(0, 10);
+        out.clear();
+        assertEquals(0, out.size());
+    }
+    @Test
+    public void shouldBeEqualWhenToArray() {
+        List<Integer> list = new ArrayList<>();
+        list.add( 10);
+        list.toArray();
+        assertEquals(10 , list.get(0));
+    }
+}
 
